48c5ff73fb33765df4d97f5bc1f1c6ae9ba97673
Brian Rosner <brian@brosner.com>
2017-06-09 17:46:05 -0600

    better practices

---
+++ src/index.js
@@ -0,0 +1,76 @@
+const fetch = require('isomorphic-fetch');
+const withQuery = require('with-query');
+
+
+function authHeaders(accessToken) {
+  return {
+    Authorization: `Bearer ${accessToken}`,
+  };
+}
+
+class Client {
+
+  constructor({ cluster }) {
+    this.apiUrl = `https://api.${cluster}.eldarioncloud.com/v2/`;
+  }
+
+  resourceGroups({ accessToken }) {
+    const client = this;
+    return {
+      async list() {
+        const res = await fetch(`${client.apiUrl}resource_groups/`, {
+          headers: Object.assign({}, authHeaders(accessToken)),
+        });
+        return res.json();
+      },
+    };
+  }
+
+  sites({ accessToken, resourceGroup }) {
+    const client = this;
+    return {
+      async list() {
+        const res = await fetch(
+          withQuery(`${client.apiUrl}sites/`, { resource_group: resourceGroup }),
+          {
+            headers: Object.assign({}, authHeaders(accessToken)),
+          },
+        );
+        return res.json();
+      },
+    };
+  }
+
+  instances({ accessToken, site }) {
+    const client = this;
+    return {
+      async list() {
+        const res = await fetch(
+          withQuery(`${client.apiUrl}instances/`, { site }),
+          {
+            headers: Object.assign({}, authHeaders(accessToken)),
+          },
+        );
+        return res.json();
+      },
+    };
+  }
+
+  services({ accessToken, instance }) {
+    const client = this;
+    return {
+      async list() {
+        const res = await fetch(
+          withQuery(`${client.apiUrl}services/`, { instance }),
+          {
+            headers: Object.assign({}, authHeaders(accessToken)),
+          },
+        );
+        return res.json();
+      },
+    };
+  }
+
+}
+
+module.exports = Client;
